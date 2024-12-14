7265c811c181e3752ea00f53aba3a5dc3334070d
artlaskavyi <art.laskavyi@gmail.com>
2024-12-14 02:07:03 +0200

    added handling env variables

--- src/main/java/com/arty/musicservice/MusicServiceApplication.java
+++ src/main/java/com/arty/musicservice/MusicServiceApplication.java
@@ -6,12 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicServiceApplication {
+
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().directory("/Users/user/IdeaProjects/MusicService/").load();
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+        // Check if the active profile is 'prod'
+        String activeProfile = System.getenv("SPRING_PROFILES_ACTIVE");
+
+        if (activeProfile == null || !activeProfile.equals("prod")) {
+            // In non-prod profiles, load .env file for local development
+            Dotenv dotenv = Dotenv.configure().directory("/Users/user/IdeaProjects/MusicService/").load();
+            System.setProperty("DB_URL", dotenv.get("DB_URL"));
+            System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+            System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+            System.setProperty("DB_DRIVER", dotenv.get("DB_DRIVER"));
+            System.out.println("Loaded environment variables from .env file for local development.");
+        }
+        // In production, environment variables are expected to be set
+        // do nothing
 
         SpringApplication.run(MusicServiceApplication.class, args);
     }
